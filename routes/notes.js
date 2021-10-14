b6056d7dbfaa8859b72546bea181eb698bd08107
MaxHuw <m.huw.graham@gmail.com>
2021-10-14 14:18:30 -0400

    Param error handling and start of Sort functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,18 @@
 export function list(req, res) {
+  let { sort } = req.query;
+  sort = sort ? sort.toLowerCase() : "desc";
+  if (!(sort === "asc" || sort === "desc")) {
+    return res.status(400).send("Invalid sort param.")
+  };
+  console.log({ sort })
   res.json([]);
 }
 
 export function create(req, res) {
   const { title, body }  = req.body;
+  if ( title === undefined || body === undefined) {
+    return res.status(400).send("Missing title or body");
+  }
   console.log(`${title} and ${body} recieved`)
   res.send("Note Created.")
 }
@@ -19,6 +28,9 @@ export function read(req, res){
 export function update(req, res) {
   const { id } = req.params;
   const { title, body } = req.body;
+  if (title === undefined && body === undefined) {
+    return res.status(400).send("Missing title and/or body.")
+  }
   console.log(`Updating ${id} with ${title} and ${body}`)
   res.send("Note Updated");
 }
